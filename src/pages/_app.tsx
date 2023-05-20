649b382da7b6dab45fccff400b67b4c86c7ee403
Trevor <kfchut12345@gmail.com>
2023-05-20 13:10:09 -0700

    Refactor landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,23 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+const theme = createTheme({
+  type: "dark",
+})
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-  <NextUIProvider>
+  <NextUIProvider theme={theme}>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
